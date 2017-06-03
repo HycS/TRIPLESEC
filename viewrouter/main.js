35fa621fd2e4b4ab030d2027de8f027f66084260
JongBinKang <krteng93@gmail.com>
2017-06-03 20:42:11 +0900

    init

--- viewrouter/main.js
+++ viewrouter/main.js
@@ -3,10 +3,14 @@ module.exports = function(app)
      app.get('/',function(req,res){
         res.render('main.html')
      });
-     app.get('/test.html',function(req,res){
-        res.render('test.html');
+     app.get('/calendar.html',function(req,res){
+        res.render('calendar.html');
     });
     app.get('/map.html',function(req,res){
         res.render('map.html');
     });
- }
+    app.get('/main.html',function(req,res){
+        res.render('main.html');
+    });
+  
+}
