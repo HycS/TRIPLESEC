1205332a79a3e2109db5c9a4c9de663edad4f2bc
JongBinKang <krteng93@gmail.com>
2017-06-03 21:15:16 +0900

    init

--- viewrouter/main.js
+++ viewrouter/main.js
@@ -1,16 +1,9 @@
 module.exports = function(app)
 {
      app.get('/',function(req,res){
-        res.render('main.html');
+        res.render('main.html')
      });
      app.get('/calendar.html',function(req,res){
         res.render('calendar.html');
     });
-    app.get('/map.html',function(req,res){
-        res.render('map.html');
-    });
-    app.get('/main.html',function(req,res){
-        res.render('main.html');
-    });
-  
 }
