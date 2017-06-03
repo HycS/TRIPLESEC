05c304e7407d3c8fcb3f13b8306ace0472e1fa04
JongBinKang <krteng93@gmail.com>
2017-06-03 21:28:36 +0900

    init

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require('express');
-
 var app = express();
 var viewrouter = require('./viewrouter/main')(app);
-
+let port = process.env.PORT || 3000;
 app.set('views', __dirname + '/views');
 
 app.set('view engine', 'ejs');
 
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(3000, function(){
+var server = app.listen(port, function(){
 
     console.log("Express server has started on port 3000")
 
